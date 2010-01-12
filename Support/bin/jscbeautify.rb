099e21a909d7a306573ea20a2e90eb804e0c74b8
Adam Hope <adam_hope@Adam-Hopes-MacBook-Pro.local>
2010-01-12 14:16:51 +1100

    JS Beautifier no longer loses unsaved changes

--- Support/bin/jscbeautify.rb
+++ Support/bin/jscbeautify.rb
@@ -10,21 +10,16 @@ JSC           = "#{SUPPORT}/bin/JavaScriptCore.framework/Resources/jsc"
 BEAUTIFY      = "#{SUPPORT}/lib/beautify.js"
 LAUNCHER      = "#{SUPPORT}/bin/beautifier.js"
 
-# BINARY   = `uname -a` =~ /i386/ ? "#{SUPPORT}/bin/intel/jsl" : "#{SUPPORT}/bin/ppc/jsl"
+# INPUT is the content of the file, regardless of whether it's been saved
+INPUT = ARGF.read
 
-if (SELECTED_TEXT)
-    # pass the selected text options as arguments to the launcher
-    sel     = "#{SELECTED_TEXT}"
-    path    = ENV['TM_BUNDLE_SUPPORT'] +  "/jsc-tmp" + rand(1000000).to_s + ".js"
-    my_file = File.new(path,"w+") # create a file object
-    my_file.puts(sel)             # put the selection in it
-    my_file.flock(File::LOCK_UN)  # unlock the file
-    output = `"#{JSC}" "#{BEAUTIFY}" "#{LAUNCHER}" -- "{file: '#{path}', indent_size: #{TAB_SIZE}, space_after_anon_function: true}"`
-    File.delete(path)
-else
-    # pass the selected file and '-f' param as an argument to the launcher
-    output = `"#{JSC}" "#{BEAUTIFY}" "#{LAUNCHER}" -- "{file: '#{FILEPATH}', indent_size: #{TAB_SIZE}, space_after_anon_function: true}"`
-end
+# pass the selected text options as arguments to the launcher
+path    = ENV['TM_BUNDLE_SUPPORT'] +  "/jsbeautifier-tmp" + rand(1000000).to_s + ".js"
+my_file = File.new(path,"w+") # create a file object
+my_file.puts(INPUT)             # put the selection in it
+my_file.flock(File::LOCK_UN)  # unlock the file
+output = `"#{JSC}" "#{BEAUTIFY}" "#{LAUNCHER}" -- "{file: '#{path}', indent_size: #{TAB_SIZE}, space_after_anon_function: true}"`
+File.delete(path)
 
 # NOTE beutifier formates "function(){}()" as "function () {} ()" which fails JSLint should be "function () {}()"
 output = output.gsub(/\}\s+\(\)/, "}()")
